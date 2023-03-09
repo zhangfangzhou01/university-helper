88165c55e38481bb377d6d09f1f2b5df77932ccc
Shinki <2633915958@qq.com>
2023-03-09 21:30:42 +0800

    删除NotNull注解

--- src/main/java/com/yhm/universityhelper/entity/po/User.java
+++ src/main/java/com/yhm/universityhelper/entity/po/User.java
@@ -11,7 +11,6 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
@@ -34,7 +33,6 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @TableId(value = "userId", type = IdType.AUTO)
-    @NotNull(message = "用户id不能为空")
     private Integer userId;
     @TableField("username")
     private String username;
