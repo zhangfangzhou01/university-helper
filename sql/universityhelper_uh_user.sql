e1bb253d0a20d7707f6dc5dbe1bd32deebf776df
wsj-wsj <2298320472@qq.com>
2023-03-23 19:22:54 +0800

    Task 改数据库字段， 适应jdk8使用新的的注解，

--- sql/universityhelper_uh_user.sql
+++ sql/universityhelper_uh_user.sql
@@ -1,4 +1,4 @@
-insert into universityhelper.uh_user (userId, username, nickname, password, sex, school, studentId, email, phone,
+insert into universityhelper.uh_user (userId, username, nickname, password, sex, ``, studentId, email, phone,
                                       location, description, avatar)
 values (1, 'zfz', 'shinki', '$2a$10$GrlK2I4l2bXieIAfdFzHNuzjZavhXxmyEKTNRkJ3Fn/88BqXv9E1m', '男', '苏州大学', 2027405037,
         '2633915958@qq.com', '123', '苏州大学', '你好啊', 0);
