0db8099afd61bc8263c5366b57dede04348ef38f
Shinki <2633915958@qq.com>
2023-03-23 23:37:55 +0800

    增加聊天模块，增加事务处理，细化数据库操作粒度

--- src/main/java/com/yhm/universityhelper/util/ReflectUtils.java
+++ src/main/java/com/yhm/universityhelper/util/ReflectUtils.java
@@ -1,105 +1,83 @@
 package com.yhm.universityhelper.util;
 
+import lombok.SneakyThrows;
+
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ReflectUtils {
+    @SneakyThrows({NoSuchFieldException.class, IllegalAccessException.class})
     public static boolean set(Object obj, String fieldName, Object value) {
-        try {
-            Field field = obj.getClass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            field.set(obj, value);
-            return true;
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            System.out.println(e.getMessage());
-            return false;
-        }
+        Field field = obj.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(obj, value);
+        return true;
     }
 
+    @SneakyThrows({NoSuchFieldException.class, IllegalAccessException.class})
     public static Object get(Object obj, String fieldName) {
-        try {
-            Field field = obj.getClass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            return field.get(obj);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
+        Field field = obj.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return field.get(obj);
     }
 
+    @SneakyThrows({NoSuchFieldException.class, IllegalAccessException.class})
     public static boolean setBatch(Object obj, Map<String, Object> data) {
-        try {
-            for (Map.Entry<String, Object> entry : data.entrySet()) {
+        for (Map.Entry<String, Object> entry : data.entrySet()) {
+            Field field = obj.getClass().getDeclaredField(entry.getKey());
+            field.setAccessible(true);
+            field.set(obj, entry.getValue());
+        }
+        return true;
+    }
+
+    @SneakyThrows({NoSuchFieldException.class, IllegalAccessException.class})
+    public static Map<String, Object> getBatch(Object obj, String... fieldNames) {
+        Map<String, Object> data = new HashMap<>();
+        for (String fieldName : fieldNames) {
+            Field field = obj.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            data.put(fieldName, field.get(obj));
+        }
+        return data;
+    }
+
+    @SneakyThrows({NoSuchFieldException.class, IllegalAccessException.class})
+    public static boolean setBatchWithout(Object obj, Map<String, Object> data, String... fieldNames) {
+        for (Map.Entry<String, Object> entry : data.entrySet()) {
+            boolean flag = true;
+            for (String fieldName : fieldNames) {
+                if (entry.getKey().equals(fieldName)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
                 Field field = obj.getClass().getDeclaredField(entry.getKey());
                 field.setAccessible(true);
                 field.set(obj, entry.getValue());
             }
-            return true;
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            System.out.println(e.getMessage());
-            return false;
         }
+        return true;
     }
 
-    public static Map<String, Object> getBatch(Object obj, String... fieldNames) {
-        try {
-            Map<String, Object> data = new HashMap<>();
-            for (String fieldName : fieldNames) {
-                Field field = obj.getClass().getDeclaredField(fieldName);
-                field.setAccessible(true);
-                data.put(fieldName, field.get(obj));
-            }
-            return data;
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
-    }
-
-    public static boolean setBatchWithout(Object obj, Map<String, Object> data, String... fieldNames) {
-        try {
-            for (Map.Entry<String, Object> entry : data.entrySet()) {
-                boolean flag = true;
-                for (String fieldName : fieldNames) {
-                    if (entry.getKey().equals(fieldName)) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) {
-                    Field field = obj.getClass().getDeclaredField(entry.getKey());
-                    field.setAccessible(true);
-                    field.set(obj, entry.getValue());
-                }
-            }
-            return true;
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            System.out.println(e.getMessage());
-            return false;
-        }
-    }
-
+    @SneakyThrows({NoSuchFieldException.class, IllegalAccessException.class})
     public static Map<String, Object> getBatchWithout(Object obj, String... fieldNames) {
-        try {
-            Map<String, Object> data = new HashMap<>();
-            for (Field field : obj.getClass().getDeclaredFields()) {
-                boolean flag = true;
-                for (String fieldName : fieldNames) {
-                    if (field.getName().equals(fieldName)) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) {
-                    field.setAccessible(true);
-                    data.put(field.getName(), field.get(obj));
+        Map<String, Object> data = new HashMap<>();
+        for (Field field : obj.getClass().getDeclaredFields()) {
+            boolean flag = true;
+            for (String fieldName : fieldNames) {
+                if (field.getName().equals(fieldName)) {
+                    flag = false;
+                    break;
                 }
             }
-            return data;
-        } catch (IllegalAccessException e) {
-            System.out.println(e.getMessage());
-            return null;
+            if (flag) {
+                field.setAccessible(true);
+                data.put(field.getName(), field.get(obj));
+            }
         }
+        return data;
     }
 }
