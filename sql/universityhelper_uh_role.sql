9d61931f915b11bcf6e309fe5e815c78af3dcc7e
wsj-wsj <2298320472@qq.com>
2023-03-23 22:38:31 +0800

    test

--- sql/universityhelper_uh_role.sql
+++
@@ -1,3 +0,0 @@
-insert into universityhelper.uh_role (roleId, rolename)
-values (1, 'ROLE_ADMIN'),
-       (2, 'ROLE_USER');
--- sql/universityhelper_uh_user_role.sql
+++
@@ -1,2 +0,0 @@
-insert into universityhelper.uh_user_role (userId, roleId, id)
-values (1, 1, 1);
