d9540603cbd2a47c878236dfe36366d0830bf951
Shinki <2633915958@qq.com>
2023-03-27 14:46:37 +0800

    完善用户和任务的事务处理

--- src/main/java/com/yhm/universityhelper/controller/WebviewController.java
+++ src/main/java/com/yhm/universityhelper/controller/WebviewController.java
@@ -8,18 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Api(tags = "页面跳转")
 @Controller
 public class WebviewController {
-    @ApiOperation(value = "用户页面", notes = "用户页面")
-    @GetMapping("/user")
-    public String user() {
-        return "user";
-    }
-
-    @ApiOperation(value = "管理员页面", notes = "管理员页面")
-    @GetMapping("/admin")
-    public String admin() {
-        return "admin";
-    }
-
     @ApiOperation(value = "主页面", notes = "主页面")
     @GetMapping("/index")
     public String hello() {
