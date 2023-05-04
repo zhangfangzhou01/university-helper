ba87889fa708f7b8b1f4b077ba59b605d226a751
Shinki <2633915958@qq.com>
2023-05-04 20:03:00 +0800

    修正聊天室在线人数识别逻辑

---
+++ sql/chat/uh_websocket_online.sql
@@ -0,0 +1,14 @@
+create table uh_websocket_online
+(
+    id         bigint auto_increment
+        primary key,
+    username   varchar(255) default ''                    not null,
+    isOnline   tinyint(1)   default 0                     not null,
+    onlineTime timestamp    default '1970-01-02 00:00:00' not null,
+    constraint uh_websocket_online_username_uindex
+        unique (username),
+    constraint username
+        unique (username)
+);
+
+INSERT INTO universityhelper.uh_websocket_online (id, username, isOnline, onlineTime) VALUES (1, '2027405037', 1, '2023-05-04 20:02:29');
--- src/main/java/com/yhm/universityhelper/config/WebSocketConfig.java
+++ src/main/java/com/yhm/universityhelper/config/WebSocketConfig.java
@@ -1,8 +1,6 @@
 package com.yhm.universityhelper.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,9 +9,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Autowired
-    private AuthChannelInterceptor authChannelInterceptor;
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic", "/queue");
@@ -24,10 +19,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(authChannelInterceptor);
     }
 }
